3b9d3acf30661e4db469612756481e664f95ed09
jkaalekei <jkaalekei@gmail.com>
2026-01-28 13:02:32 -0500

    ejercicio uno de cadenas

--- ejercicioCadenasUno/Program.cs
+++ ejercicioCadenasUno/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ejercicioCadenasUno
@@ -10,6 +11,24 @@ namespace ejercicioCadenasUno
     {
         static void Main(string[] args)
         {
+            /*
+             Ejercicio Cadenas 1: Contador de Caracteres
+                Crea un programa que ingrese una cadena de texto y el programa cuente el número de caracteres en esa cadena, incluyendo letras, números, espacios en blanco y signos de puntuación.
+
+                Usa la función Length
+
+
+             */
+
+            string texto = "Hola mundo .2#  123";
+            int cant = texto.Length;
+            
+
+            Console.WriteLine(cant);
+            Console.ReadKey();
+
+
+
         }
     }
 }
