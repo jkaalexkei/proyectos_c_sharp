a65482c42eb732d2114ec23bad25d4c718ff7b11
jkaalekei <jkaalekei@gmail.com>
2026-01-28 21:18:35 -0500

    ejercicio de palindromo

---
+++ ejemploPalindromo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploPalindromo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string palabra;
+            int longitudPalabra;
+
+            Console.WriteLine("Ingrese la palabra a validar");
+            palabra = Console.ReadLine().ToLower();//solicitamos una palabra
+
+            longitudPalabra = palabra.Length;
+            //creamos 2 arrays para almacenar la palabra ingresada y esa misma palabra de forma invertida
+            string[] palindromo = new string[longitudPalabra];
+            string[] textoInvertido = new string[longitudPalabra];
+
+            for (int i = 0; i < longitudPalabra; i++)//recorremos la palabra ingresada y la agregamos al array
+            {
+                palindromo[i] = palabra[i].ToString();//agregamos cada letra en una posicion del array
+                Console.Write(palindromo[i]);
+            }
+            Console.WriteLine();
+            int indiceInvertido = 0;//creamos este indice para guardar las letras de forma invertida
+            for (int j = longitudPalabra - 1; j >= 0; j--)
+            {
+                textoInvertido[indiceInvertido] = palabra[j].ToString();//almacenamos los caracteres de la palabra ingresada de forma invertida
+                Console.Write(textoInvertido[indiceInvertido]);
+                indiceInvertido++;
+            }
+            Console.WriteLine();
+
+            bool esPalindromo = true;
+
+            for (int p = 0; p < longitudPalabra; p++)//recorremos ambos arreglos y los comparamos
+            {
+                if (palindromo[p] != textoInvertido[p])//validamos si cada caracter en ambos arrays son iguales si consigue alguna diferencia se sale del bucle
+                {
+                    esPalindromo = false;
+                    break;
+                }
+            }
+
+            if (esPalindromo)
+            {
+                Console.WriteLine("La palabra es palindromo");
+            }
+            else
+            {
+                Console.WriteLine("La palabra no es palindromo");
+            }
+
+        }
+    }
+}
---
+++ estructuras/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estructuras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            int longitudPalabra;
+
+            Console.WriteLine("Ingrese la palabra a validar");
+            palabra = Console.ReadLine().ToLower();
+
+            longitudPalabra = palabra.Length;
+
+            string[] palindromo = new string[longitudPalabra];
+            string[] textoInvertido = new string[longitudPalabra];
+            
+            for (int i = 0; i < longitudPalabra; i++)
+            {
+                palindromo[i] = palabra[i].ToString();
+                Console.Write(palindromo[i]);
+            }
+            Console.WriteLine();
+            int indiceInvertido = 0;
+            for (int j = longitudPalabra - 1; j >=0; j--)
+            {
+                textoInvertido[indiceInvertido] = palabra[j].ToString();
+                Console.Write(textoInvertido[indiceInvertido]);
+                indiceInvertido++;
+            }
+            Console.WriteLine();
+
+            bool esPalindromo = true;
+
+            for (int p = 0; p < longitudPalabra; p++)
+            {
+                if (palindromo[p] != textoInvertido[p])
+                {
+                    esPalindromo = false;
+                    break;
+                }
+            }
+
+            if (esPalindromo)
+            {
+                Console.WriteLine("La palabra es palindromo");
+            }
+            else
+            {
+                Console.WriteLine("La palabra no es palindromo");
+            }
+            
+
+            
+
+
+
+
+        }
+    }
+}
+
+/*
+ Estructuras: se componen de muchos arreglos, se puede administrar mejor el orden
+
+Las estructuras son tipos definidos por el programador y son un conjunto de datos agrupados. funcionan como un array o parecido a las colecciones
+
+permiten flexibilidad al almacenar datos
+
+Cuando creamos una estructura, definimos un nuevo tipo y adentro de este tipo podremos colocar datos. Estos datos se conocen como campos
+
+cada campo esta representado por una variable, que puede ser de cualquier tipo
+
+Luego que hemos terminado de definir la estructura podemos crear variables de esta estructura y guardar la información que necesitamos en ellas
+
+se puede manejar como un arreglo pero ahi que tener presente los datos que se deben almacenar
+
+ejemplo:
+Agenda telefonica:
+    nombre: string
+    direccion: string
+    telefono: string
+    edad: int
+
+Definir una estructura:
+
+para definir una estructura debemos usar 3 pasos:
+
+1. el acceso: indica si la estructura puede verse por fuera del ámbito
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
