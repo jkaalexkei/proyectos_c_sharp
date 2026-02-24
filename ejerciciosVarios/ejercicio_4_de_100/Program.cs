d06fd38c46e154bcdec78970b196af76cc2aa4b7
jkaalekei <jkaalekei@gmail.com>
2026-02-24 18:42:08 -0500

    ejercicio_4_100

---
+++ ejerciciosVarios/ejercicio_4_de_100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_4_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe un programa que multiplique dos numeros enteros y muestre el resultado, concatena la palabra resultado
+
+            int n1, n2, resultado;
+            n1 = 3;
+            n2 = 4;
+            resultado = n1 * n2;
+            Console.WriteLine($"Resultado = {resultado}");
+        }
+    }
+}
