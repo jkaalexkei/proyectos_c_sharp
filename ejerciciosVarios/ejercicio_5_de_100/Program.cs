6e38455ceb66f3602b0b00cda71e3c6542a889db
jkaalekei <jkaalekei@gmail.com>
2026-02-24 18:51:44 -0500

    ejercicio_5_100

---
+++ ejerciciosVarios/ejercicio_5_de_100/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_5_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe un programa que divida
+            // dos numeros decimales literales y muestre el resultado
+
+            decimal d1, d2, resultado;
+            d1 = 23.4m;//se coloca el sufijo m para decimales literales
+            d2 = 45.7m;
+            resultado = d1 / d2;
+            Console.WriteLine($"resultado {resultado:F2}");//forma1
+            Console.WriteLine("resultado " + string.Format("{0:F2}",resultado));//forma2
+        }
+    }
+}
