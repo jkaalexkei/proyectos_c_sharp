50272cff607fe3c0cc1ac2011241c691c2df13af
jkaalekei <jkaalekei@gmail.com>
2026-02-24 18:54:18 -0500

    ejercicio_6_100

---
+++ ejerciciosVarios/ejercicio_6_de_100/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_6_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             escribe un programa
+            que calcule el modulo de dos numeros enteros
+            y muestre el resultado
+             
+             */
+            int n1, n2, resModulo;
+
+            n1 = 10;
+            n2 = 2;
+
+            resModulo = n1 % n2;
+
+            Console.WriteLine("El modulo es: {0}",resModulo);
+
+        }
+    }
+}
---
+++ ejerciciosVarios/ejercicio_6_de_100/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ejercicio_6_de_100")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ejercicio_6_de_100")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("5b5d0ba4-bfae-47a4-aac1-1401ac459935")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
