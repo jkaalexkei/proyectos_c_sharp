a2f48f9c9807ace2864b01cc8d4322cae7337411
jkaalekei <jkaalekei@gmail.com>
2026-02-24 18:38:30 -0500

    ejercicio_3_100

---
+++ ejerciciosVarios/ejercicio_2_de_100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe un programa que sume dos numeros enteros y muestre el resultado
+
+            int n1, n2, r;
+            n1 = 10;
+            n2 = 20;
+            r = n1 + n2;
+            Console.WriteLine($"La suma es: {r}");
+        }
+    }
+}
---
+++ ejerciciosVarios/ejercicio_3_de_100/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             escribe un programa que reste dos numeros decimales y muestre el resultado
+             */
+
+            double n1, n2, r;
+            n1 = 93.6;
+            n2 = 45.8;
+            r = n1 - n2;
+            Console.WriteLine(r);
+        }
+    }
+}
