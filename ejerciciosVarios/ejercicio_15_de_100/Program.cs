f7d30b686baccf3a55bacaa0fd500c8224d6125a
jkaalekei <jkaalekei@gmail.com>
2026-02-24 22:09:03 -0500

    ejercicio_15_de_100

---
+++ ejerciciosVarios/ejercicio_15_de_100/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_15_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //convierte una variable de tipo int a float y viceversa
+            int entero = 10;
+            float flotante = 15.5f;
+            Console.WriteLine(entero.GetType());
+            
+            Console.WriteLine(flotante.GetType());
+
+            float enteroConvertido = (float)entero;//convertimos entero a float
+            Console.WriteLine(enteroConvertido.GetType());
+
+            int flotanteConvertido = (int)flotante;//convertimos flotante a int
+            Console.WriteLine(flotanteConvertido.GetType());
+
+
+
+
+
+
+        }
+    }
+}
