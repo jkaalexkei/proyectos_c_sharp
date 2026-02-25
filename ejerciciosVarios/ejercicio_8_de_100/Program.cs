0f59b32220885e8135167845f8a01ae9eb58b0ca
jkaalekei <jkaalekei@gmail.com>
2026-02-24 20:01:04 -0500

    ejercicio_8_100

---
+++ ejerciciosVarios/ejercicio_8_de_100/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_8_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * escribe un programa que calcule la longitud de una cadena y muestre el resultado
+             *
+             */
+
+            string cadena = "Hola Mundo";
+            int longitud = cadena.Length;
+            Console.WriteLine("La cadena tiene: {0} caracteres",longitud);
+        }
+    }
+}
