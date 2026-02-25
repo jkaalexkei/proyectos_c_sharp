6de1c671a391ea1c0a273dfe49e429c391dc667c
jkaalekei <jkaalekei@gmail.com>
2026-02-24 20:42:02 -0500

    ejercicio_13_de_100

---
+++ ejerciciosVarios/ejercicio_13_de_100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_13_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedir al usuario un numero de punto flotante, multiplicalo por dos y muestra el resultado
+            double numero;
+            double resultado;
+            Console.WriteLine("Ingresa un numero decimal: ");
+            numero = Convert.ToDouble(Console.ReadLine());
+            resultado = numero * 2;
+            Console.WriteLine(resultado);
+        }
+    }
+}
