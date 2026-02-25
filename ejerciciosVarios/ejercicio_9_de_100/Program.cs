43e835e1a83b389b856335a1c3e642ef89e27c2d
jkaalekei <jkaalekei@gmail.com>
2026-02-24 20:08:58 -0500

    ejercicio_9_100

---
+++ ejerciciosVarios/ejercicio_9_de_100/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_9_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * escribe un programa que convierta una cadena a mayusculas y muestre el resultado
+             */
+
+            string cadena;
+            string cadenaMayusculas;
+
+            Console.Write("Ingrese una frase corta: ");
+            cadena = Console.ReadLine();
+            cadenaMayusculas = cadena.ToUpper();
+            Console.Write(cadenaMayusculas);
+        }
+    }
+}
