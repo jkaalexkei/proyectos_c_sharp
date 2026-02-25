41c8b1577a81e90e98e61193421314131a4b2553
jkaalekei <jkaalekei@gmail.com>
2026-02-24 20:15:10 -0500

    ejercicio_11_100

---
+++ ejerciciosVarios/ejercicio_11_de_100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_11_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*pedir al usuario un string por teclado e imprime el resultado*/
+            String cadena;
+            Console.WriteLine("Ingresa una frase");
+            cadena = Console.ReadLine();
+
+            Console.WriteLine("La cadena ingresada es: {0}",cadena);
+        }
+    }
+}
