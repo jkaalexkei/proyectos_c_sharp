70338cae3507fbd7d97622b81e0ba55988361bd6
jkaalekei <jkaalekei@gmail.com>
2026-02-24 22:12:02 -0500

    ejercicio_16_de_100

---
+++ ejerciciosVarios/ejercicio_16_de_100/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_16_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declara una variable con tipo de dato implicito y obtener el tipo de dato de la misma
+
+            var variable = 10;//asi declaramos una variable con tipo de dato implicito usando var
+
+            Console.WriteLine(variable.GetType());
+        }
+    }
+}
