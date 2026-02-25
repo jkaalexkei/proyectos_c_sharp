702abc096119d179832e4d3bffe99f4d1da622a5
jkaalekei <jkaalekei@gmail.com>
2026-02-24 22:21:16 -0500

    ejercicio_17_de_100

---
+++ ejerciciosVarios/ejercicio_17_de_100/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_17_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declara una constante y asignale vale
+
+            //las constantes son utiles cuando tienes vaores que no deben cambiar, como constantes matematicas, configuraciones fijas, etc.
+
+            const double pi = 3.1416;
+
+            Console.WriteLine(pi);
+            
+         
+
+        }
+    }
+    
+}
