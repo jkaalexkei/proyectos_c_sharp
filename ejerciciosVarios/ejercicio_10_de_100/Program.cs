197a78ea80e84a9c8f2bd008a3b6e28626064f12
jkaalekei <jkaalekei@gmail.com>
2026-02-24 20:12:22 -0500

    ejercicio_10_100

---
+++ ejerciciosVarios/ejercicio_10_de_100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_10_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*escribe un programa que convierta una cadena a minusculas y muestre el resultado*/
+
+            string cadena = "HOLA MUNDO";
+            string cadenaMinusculas;
+            Console.WriteLine(cadena);
+            cadenaMinusculas = cadena.ToLower();
+            Console.WriteLine(cadenaMinusculas);
+        }
+    }
+}
