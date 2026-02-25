6736896909fe6fec289129f3f02a7bbaac649a44
jkaalekei <jkaalekei@gmail.com>
2026-02-24 19:55:21 -0500

    ejercicio_7_100

---
+++ ejerciciosVarios/ejercicio_7_de_100/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_7_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             escribe un programa que concatene dos cadenas y muestre el resultado
+             
+             */
+
+            string cadena1, cadena2;
+            cadena1 = "Hola";
+            cadena2 = "Mundo";
+            Console.WriteLine(cadena1 + " " + cadena2);
+
+        }
+    }
+}
