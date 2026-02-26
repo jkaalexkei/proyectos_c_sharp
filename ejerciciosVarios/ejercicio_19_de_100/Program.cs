a36ce9a58172a0f642c2482e70b2199f0b8e987c
jkaalekei <jkaalekei@gmail.com>
2026-02-25 19:24:59 -0500

    ejercicio_19_de_100

---
+++ ejerciciosVarios/ejercicio_19_de_100/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_19_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;//Esto permite mostrar correctamente los caracteres Unicode.
+            /*declara una variable de tipo DateTime y asignale la fecha y hora actual*/
+            DateTime miFecha = DateTime.Now;//variable de tipo fecha
+            Console.WriteLine(miFecha);
+        }
+    }
+}
