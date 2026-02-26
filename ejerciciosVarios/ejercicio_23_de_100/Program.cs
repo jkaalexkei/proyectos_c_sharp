3ad075b89efd036050562e92d44a3499218a2085
jkaalekei <jkaalekei@gmail.com>
2026-02-25 19:46:23 -0500

    ejercicio_23_de_100

---
+++ ejerciciosVarios/ejercicio_23_de_100/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_23_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe un programa que clasifique un numero como positivo, negativo o cero
+
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            //aqui se utilizo una validacion con operador ternario anidado
+            string comprobacion = (numero > 0) ? "Positivo" :
+                                   (numero < 0) ? "Negativo" : "Es Cero";
+
+            Console.WriteLine(comprobacion);
+        }
+    }
+}
