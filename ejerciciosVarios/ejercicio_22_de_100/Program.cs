74c2e0555473d60b11ca3880e6bcf30c1ae56f7d
jkaalekei <jkaalekei@gmail.com>
2026-02-25 19:40:09 -0500

    ejercicio_22_de_100

---
+++ ejerciciosVarios/ejercicio_22_de_100/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_22_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe un programa que verifique si un numero es par o impar
+            int numero = Convert.ToInt32(Console.ReadLine());
+            string comprobacion = (numero % 2 == 0) ? "Par" : "Impar";
+
+            Console.WriteLine(comprobacion);
+        }
+    }
+}
