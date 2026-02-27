7625600e7e78ef532a8b6bf2f10e94898f568ed9
jkaalekei <jkaalekei@gmail.com>
2026-02-26 20:27:19 -0500

    ejercicio_26_de_100

---
+++ ejerciciosVarios/ejercicio_26_de_100/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_26_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Escribe un programa que compare dos cadenas y muestre un mensaje si estas son iguales
+             */
+
+            string cadena1 = "Hola";
+            string cadena2 = "hola";
+
+            if (cadena1 == cadena2)
+            {
+                Console.WriteLine("Las cadenas son iguales");
+            }
+            else
+            {
+                Console.WriteLine("No son iguales");
+            }
+        }
+    }
+}
