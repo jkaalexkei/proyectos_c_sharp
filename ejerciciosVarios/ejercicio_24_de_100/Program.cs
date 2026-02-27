3c765e884e9559fbc3eee33b50bc152ba2649dad
jkaalekei <jkaalekei@gmail.com>
2026-02-26 20:14:21 -0500

    ejercicio_24_de_100

--- ejerciciosVarios/ejercicio_24_de_100/Program.cs
+++ ejerciciosVarios/ejercicio_24_de_100/Program.cs
@@ -10,66 +10,19 @@ namespace ejercicio_24_de_100
     {
         static void Main(string[] args)
         {
-            //comprobar un numero random
 
-            Random numero = new Random();
-            int sorteoNumero = numero.Next(20);
-            //Console.WriteLine(sorteoNumero);
-            bool validacion = true;
-            int intentos = 3;
-            do {
+            //usar el operador ternario para determinar si un numero es par o impar
 
-                Console.WriteLine("Adivine el numero");
-                int valor = Convert.ToInt32(Console.ReadLine());
-                if (valor > sorteoNumero)
-                {
-                    Console.WriteLine("El numero es menor, te quedan {0} intentos",intentos);
-                    intentos--;
-                }
-                else if (valor < sorteoNumero)
-                {
-                    Console.WriteLine("El numero es mayor te quedan {0} intentos", intentos);
-                    intentos--;
-                }
-                else
-                {
-                    Console.WriteLine("Haz adivinado el numero en {0} intentos",intentos);
-                    validacion = false;
-                }
+            //operador ternario: es una forma compacta de escribir una condicion
 
-                if(intentos ==0)
-                {
-                    validacion = false;
-                    Console.WriteLine("No lograste adivinar el numero, alcanzaste el numero de intentos");
-                }
-                
+            int numero = 7;
 
+            string validacion = (numero % 2 == 0) ? "Es Par" : "Es Impar";
 
-
-            } while (validacion);
-            
+            Console.WriteLine(validacion);
            
             
-            //programa que valide si un numero impar, en caso que sea par se salga de la aplicacion
-
-            //int numero = 0;
-            //bool validacion = true;
-            //do
-            //{
-            //    Console.WriteLine("Ingrese un numero para validar si par o impar: ");
-            //    numero = Convert.ToInt32(Console.ReadLine());
-            //    if(numero % 2 == 0)
-            //    {
-            //        validacion = false;
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("El numero es impar, intente otra vez");
-            //    }
-
-
-            //} while (validacion);
-            //Console.WriteLine("conseguiste un numero par");
+            
            
         }
     }
