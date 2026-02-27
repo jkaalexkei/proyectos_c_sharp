2bc7efa720d489cd95e349a68cc8052940bbced6
jkaalekei <jkaalekei@gmail.com>
2026-02-26 21:05:36 -0500

    ejercicio_27_de_100

---
+++ ejerciciosVarios/ejercicio_27_de_100/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_27_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Escribe un programa que use switch para determinar el dia la semana basado en un numero
+             */
+
+            Console.WriteLine("Ingrese el numero de dia a validar de 1 a 7 donde 1 es lunes y 7 es domingo");
+
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            switch (numero)
+            {
+                case 1:
+                    Console.WriteLine("El dia es Lunes");
+                    break;
+                case 2:
+                    Console.WriteLine("el dia es martes");
+                    break;
+                case 3:
+                    Console.WriteLine("el dia es miercoles");
+                    break;
+                case 4:
+                    Console.WriteLine("el dia es jueves");
+                    break;
+                case 5:
+                    Console.WriteLine("el dia es viernes");
+                    break;
+                case 6:
+                    Console.WriteLine("el dia es sabado");
+                    break;
+                case 7:
+                    Console.WriteLine("el dia es domingo");
+                    break;
+                default:
+                    Console.WriteLine("La opcion no esta contemplada");
+                    break;
+               
+
+            }
+        }
+    }
+}
---
+++ ejerciciosVarios/ejercicio_27_de_100/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ejercicio_27_de_100")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ejercicio_27_de_100")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("6f985d7f-1f1e-4d64-884d-0b00e3223f12")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
