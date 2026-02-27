396f9e4b48272df2ee55c8d6baca9cc9c7ecfa48
jkaalekei <jkaalekei@gmail.com>
2026-02-26 20:22:04 -0500

    ejercicio_25_de_100

---
+++ ejerciciosVarios/ejercicio_25_de_100/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_25_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             escribe un programa que verifique si un numero es mayor que 10 y menor que 20
+             */
+
+            Console.WriteLine("Ingrese un numero entre 10 y 20");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if(numero > 10 && numero < 20)
+            {
+                Console.WriteLine("El numero esta entre 10 y 20");
+            }
+            else
+            {
+                Console.WriteLine("el numero esta fuera del rango indicado");
+            }
+        }
+    }
+}
