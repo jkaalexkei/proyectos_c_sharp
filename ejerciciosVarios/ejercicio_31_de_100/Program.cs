188d3fe8a229d8f0ff5018c624420e68dab3d8e9
jkaalekei <jkaalekei@gmail.com>
2026-02-27 20:53:50 -0500

    ejercicio_31_de_100

---
+++ ejerciciosVarios/ejercicio_31_de_100/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_31_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*escribe un programa que imprima los numeros del 1 al 10 usando un bucle for */
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i+1);
+            }
+        }
+    }
+}
