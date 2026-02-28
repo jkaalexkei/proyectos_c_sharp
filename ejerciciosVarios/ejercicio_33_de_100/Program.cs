b77dc917062bba9268f749de946cd9e89f261d5b
jkaalekei <jkaalekei@gmail.com>
2026-02-27 21:02:33 -0500

    ejercicio_33_de_100

---
+++ ejerciciosVarios/ejercicio_33_de_100/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_33_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             escribe un programa que imprima 
+            los numeros del 1 al 10
+            usando un bucle do while
+             */
+
+            int contador = 1;
+            do
+            {
+                Console.WriteLine(contador);
+                contador++;
+            }
+            while (contador <=10);
+        }
+    }
+}
