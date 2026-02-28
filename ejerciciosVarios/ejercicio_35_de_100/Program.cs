f2adc9e22990ba5baff3c0dcfde052d6284f7854
jkaalekei <jkaalekei@gmail.com>
2026-02-28 15:00:48 -0500

    ejercicio_35_de_100

--- ejerciciosVarios/ejercicio_35_de_100/Program.cs
+++ ejerciciosVarios/ejercicio_35_de_100/Program.cs
@@ -17,7 +17,7 @@ namespace ejercicio_35_de_100
             int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
             foreach (int item in numeros)
-            {
+            { 
                 Console.WriteLine(item);
             }
 
