061298c7bf93b7abf18e2ac71a0ae7a299c74572
jkaalekei <jkaalekei@gmail.com>
2026-02-28 15:00:27 -0500

    ejercicio_34_de_100

---
+++ ejerciciosVarios/ejercicio_34_de_100/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_34_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             escribe un programa que imprima una tabla de multiplicar usando for
+             */
+            int tabla = 3;
+            Console.WriteLine($"tabla del {tabla}");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i+1} x {tabla} = {(i+1)*tabla}");
+            }
+        }
+    }
+}
---
+++ ejerciciosVarios/ejercicio_35_de_100/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_35_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             escribir un programa que recorra un array de numeros usando foreach
+             */
+
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            foreach (int item in numeros)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("fin");
+        }
+    }
+}
