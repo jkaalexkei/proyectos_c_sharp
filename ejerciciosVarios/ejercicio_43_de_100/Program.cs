5aeae1a7809a9c5666be20011161a549d5474cc1
jkaalekei <jkaalekei@gmail.com>
2026-03-01 13:58:06 -0500

    ejercicio_43_de_100

---
+++ ejerciciosVarios/ejercicio_43_de_100/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_43_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //programa que sume todos los elementos de un array de enteros
+
+            int[] enteros = { 1,2,3,4,5,6,7,8};
+            int suma = 0;
+
+            for (int i = 0; i < enteros.Length;i++)
+            {
+                suma += enteros[i];
+            }
+
+            Console.WriteLine("La suma de elementos del array es: {0}",suma);
+
+        }
+    }
+}
