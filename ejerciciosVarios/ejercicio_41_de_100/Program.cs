a63272c5efd541e1cf2108e074f35e251f524136
jkaalekei <jkaalekei@gmail.com>
2026-03-01 01:42:08 -0500

    ejercicio_41_de_100

---
+++ ejerciciosVarios/ejercicio_41_de_100/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_41_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             declarar un array de enteros y asignale valores
+             */
+
+            Console.WriteLine("Cuantos numeros quieres almacenar");
+            int valores = Convert.ToInt32(Console.ReadLine());
+            int[] numero = new int[valores];
+            Console.WriteLine("Ingresa los numeros a registrar: ");
+            for (int i = 0; i < numero.Length;i++)
+            {
+                Console.Write($"Valor {i+1}: ");
+                numero[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Valores registrados");
+            foreach (int items in numero)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
