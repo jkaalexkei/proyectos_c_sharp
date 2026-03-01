898d6aece87e97bbde6661d8de59ec4b5213a65d
jkaalekei <jkaalekei@gmail.com>
2026-03-01 15:57:41 -0500

    ejercicio_48_de_100

---
+++ ejerciciosVarios/ejercicio_48_de_100/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_48_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             sumar todos los elementos de una lista
+             */
+
+            List<int> listNumeros = new List<int> { 10,30};
+
+            int suma = 0;
+            Console.WriteLine("Los elementos de la lista es: ");
+            foreach (int item in listNumeros)
+            {
+                Console.WriteLine(item);
+                suma += item;
+            }
+            Console.WriteLine($"se sumaron {listNumeros.Count} elementos");
+            Console.WriteLine($"la suma total es: {suma}");
+        }
+    }
+}
