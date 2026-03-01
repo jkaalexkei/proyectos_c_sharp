577d706e008af229bba282ecfb48766e3a955f1c
jkaalekei <jkaalekei@gmail.com>
2026-02-28 20:05:29 -0500

    ejercicio_38_de_100

---
+++ ejerciciosVarios/ejercicio_38_de_100/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_38_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             escribe un programa que entre en un bucle infinito y salga con break;
+             */
+            int numero = 0;
+
+            bool condicion = true;
+
+            while(condicion)
+            {
+                Console.WriteLine("Ingrese un numero a validar: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                if(numero > 5)
+                {
+                    condicion = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("intente otra vez");
+                }
+            }
+            
+          
+        }
+    }
+}
