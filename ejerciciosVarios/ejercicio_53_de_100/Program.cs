d10e54bfd00c98731eabbc33b9a339a682a497cd
jkaalekei <jkaalekei@gmail.com>
2026-03-02 02:04:23 -0500

    ejercicio_53_de_100

---
+++ ejerciciosVarios/ejercicio_53_de_100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_53_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe una funcion que devuelva el cuadrado de un numero
+            Console.WriteLine(cuadrado(9));
+        }
+
+        public static int cuadrado(int numero)
+        {
+            return numero * numero;
+        }
+    }
+}
