7c9670dbfb98517c8541249d9388c1c12cb649f0
jkaalekei <jkaalekei@gmail.com>
2026-03-02 01:40:23 -0500

    ejercicio_50_de_100

--- ejerciciosVarios/ejercicio_50_de_100/Program.cs
+++ ejerciciosVarios/ejercicio_50_de_100/Program.cs
@@ -10,7 +10,7 @@ namespace ejercicio_50_de_100
     {
         static void Main(string[] args)
         {
-            //ordena una lista de enteros de menor a mayor
+            //ordena una lista de enteros de menor a mayor 
 
             List<int> list = new List<int> { 9,5,2,1,8};
             Console.WriteLine("lista desordenada: {0}", string.Join(",", list));
