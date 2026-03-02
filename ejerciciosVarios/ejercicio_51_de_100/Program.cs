60342daaa6b8b1d747083ffd168590db25a17b97
jkaalekei <jkaalekei@gmail.com>
2026-03-02 01:51:44 -0500

    ejercicio_51_de_100

---
+++ ejerciciosVarios/ejercicio_51_de_100/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_51_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe una funcion que sume dos numeros y devuelva el resultado
+            Console.WriteLine("Ingrese dos numeros a sumar: ");
+            Console.Write("Numero 1: "); 
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Numero 2: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("La suma de los numeros es: {0}",sumaNumeros(a,b));
+        }
+
+        static public int sumaNumeros(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
