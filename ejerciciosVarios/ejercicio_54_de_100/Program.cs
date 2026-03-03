7d7b392ae697375e54c2222b88cd02724ec52b73
jkaalekei <jkaalekei@gmail.com>
2026-03-02 20:04:09 -0500

    ejercicio_54_de_100

---
+++ ejerciciosVarios/ejercicio_54_de_100/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_54_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe una funcion que tome un parametro opcional o parametro por defecto
+
+
+            Console.WriteLine("Hola " + Saludar("Alex"));
+            Console.WriteLine("Hola " + Saludar());
+
+        }
+
+        public static string Saludar(string valor = "Sin Nombre")//funcion con parametro por defecto
+        {
+            return valor;
+        }
+    }
+}
