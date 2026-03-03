9392a272e875b0040b4c3b7c3f5db0958f70b0cd
jkaalekei <jkaalekei@gmail.com>
2026-03-02 21:06:22 -0500

    ejercicio_56_de_100

---
+++ ejerciciosVarios/ejercicio_56_de_100/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_56_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //escribe una funcion recursiva que calcule el factorial de un numero
+            //funcion recursiva es una funcion que se llama a si misma
+
+            Console.WriteLine(factorial(5));
+        }
+
+        public static int factorial(int numero)
+        {
+            if(numero <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return numero * factorial(numero - 1);//aqui se aplica la recursividad
+            }
+        }
+    }
+}
