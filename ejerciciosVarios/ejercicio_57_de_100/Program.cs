11ba3faf1f1f274594a3ca24a0e2c59d36517a31
jkaalekei <jkaalekei@gmail.com>
2026-03-03 18:53:55 -0500

    ejercicio_57_de_100

---
+++ ejerciciosVarios/ejercicio_57_de_100/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_57_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe dos funciones con el mismo nombre pero diferentes parametros
+
+            int a = 10;
+            int b = 20;
+            int c = 30;
+
+            Console.WriteLine("total suma de 2 numeros: {0}",suma(a,b ));
+            Console.WriteLine("total suma de 3 numeros: {0}",suma(a,b,c ));
+
+        }
+
+        //metodo sobrecargados
+        public static int suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int suma(int a, int b, int c)//sobrecarga
+        {
+            return a + b + c;
+        }
+    }
+}
