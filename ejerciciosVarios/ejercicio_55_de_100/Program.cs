8979c02b2301e2b9c8099123655e16261097ffde
jkaalekei <jkaalekei@gmail.com>
2026-03-02 20:23:26 -0500

    ejercicio_55_de_100

---
+++ ejerciciosVarios/ejercicio_55_de_100/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_55_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe una funcion que use parametros nombrados
+
+            datosPersonales(edad:42,nombre:"alex");//sintaxis de un parametro nombrado, sirven para una mejor lectura del codigo 
+            //sintaxis: nombrefuncion(nombredelparametro: valordelparametro);
+        }
+
+        public static void datosPersonales(string nombre, int edad) {
+
+            Console.WriteLine($"Datos: {edad} - nombre: {nombre}");
+        }
+    }
+}
