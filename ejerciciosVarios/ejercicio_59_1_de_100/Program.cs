507a7b6f56435b8f0899e15e83cf8f1e41c15e88
jkaalekei <jkaalekei@gmail.com>
2026-03-09 20:09:42 -0500

    ejercicio_59_1_de_100 paramtros por referencia e invertir numeros

--- ejerciciosVarios/ejercicio_59_1_de_100/Program.cs
+++ ejerciciosVarios/ejercicio_59_1_de_100/Program.cs
@@ -15,13 +15,37 @@ namespace ejercicio_59_1_de_100
             int num = 5;
             incrementarNumero(ref num);
             Console.WriteLine($"desde el metodo Main: {num}");
-            
+
+
+            Console.WriteLine("Ingrese un numero a invertir");
+            int dato = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"El numero invertido de {dato} es {invertirNumero(dato)}");
+
         }
 
         static void incrementarNumero(ref int numero)
         {
             numero = numero + 1;
             Console.WriteLine("Metodo: {0}",numero);
+
+
+
+        }
+
+        static int invertirNumero(int numero)
+        {
+            int invertido = 0;
+            int residuo = 0;
+
+            while (numero > 0)
+            {
+                residuo = numero % 10;
+                invertido = invertido * 10 + residuo;
+                numero = numero / 10;
+            }
+
+            return invertido;
         }
 
 
