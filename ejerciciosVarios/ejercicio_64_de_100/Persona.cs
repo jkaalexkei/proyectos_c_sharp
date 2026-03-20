2a3474c8de55308cf94652f7e09ac65d34e1cdd7
jkaalekei <jkaalekei@gmail.com>
2026-03-20 11:04:01 -0500

    ejercicio_64_de_100

---
+++ ejerciciosVarios/ejercicio_64_de_100/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_64_de_100
+{
+    class Persona
+    {
+        public string nombre {  get; set; }
+        
+        public Persona(string nombre)//constructor
+        {
+            this.nombre = nombre;
+        }
+
+        public void saludar()
+        {
+            Console.WriteLine("hola {0}",this.nombre);
+        }
+    }
+}
---
+++ ejerciciosVarios/ejercicio_64_de_100/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_64_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona("Alex");
+            persona.saludar();
+        }
+    }
+}
