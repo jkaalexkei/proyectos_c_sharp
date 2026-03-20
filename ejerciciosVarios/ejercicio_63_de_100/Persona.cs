c7277281772128702a17c17b2c078c942d9a058c
jkaalekei <jkaalekei@gmail.com>
2026-03-20 10:59:10 -0500

    ejercicio_63_de_100

---
+++ ejerciciosVarios/ejercicio_63_de_100/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_63_de_100
+{
+    internal class Persona
+    {
+        public string nombre {  get; set; }
+        public int edad {  get; set; }
+
+        public void saludar()
+        {
+            Console.WriteLine($"Hola {this.nombre}");
+        }
+    }
+}
---
+++ ejerciciosVarios/ejercicio_63_de_100/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_63_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.nombre = "alex";
+            persona.saludar();
+        }
+    }
+}
