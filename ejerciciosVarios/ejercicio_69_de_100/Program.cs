84cc18cc7b4c47c5588d70000d9938a0171c6a6f
jkaalekei <jkaalekei@gmail.com>
2026-03-21 12:43:57 -0500

    ejercicio_69_de_100

---
+++ ejerciciosVarios/ejercicio_69_de_100/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_69_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear clase Vehiculo y clase Hija Auto, con metodo MostrarTipo sobreescrito
+            Vehiculo auto = new Vehiculo();
+            auto.MostrarTipo();
+
+            Vehiculo carro = new Auto();// aqui se aplica polimorfismo
+            carro.MostrarTipo();
+        }
+
+    }
+
+    class Vehiculo
+    {
+        public int Ruedas { get; set; }
+        public string Tapiceria { get; set; }
+        
+        public Vehiculo()
+        {
+            this.Ruedas = 4;
+            this.Tapiceria = "cuero";
+        }
+        public virtual void MostrarTipo()
+        {
+            Console.WriteLine($"El Vehiculo tiene, ruebas: {Ruedas} tapiceria: {Tapiceria}");
+        }
+    }
+
+    class Auto : Vehiculo
+    {
+        public Auto()
+        {
+            this.Ruedas = 4;
+            this.Tapiceria = "Tela";
+        }
+        public override void MostrarTipo()
+        {
+            Console.WriteLine($"El auto tiene, ruedas: {this.Ruedas} tapiceria: {this.Tapiceria}");
+        }
+    }
+}
