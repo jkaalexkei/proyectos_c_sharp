ba231e2ada36bf17ea8a29bd7ac4a90479a0de2d
jkaalekei <jkaalekei@gmail.com>
2026-03-21 19:10:38 -0500

    ejercicio_72_de_100

---
+++ ejerciciosVarios/ejercicio_72_de_100/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_72_de_100
+{
+    class Convertir
+    {
+        public void Calcular(string numText)
+        {
+            try
+            {
+                int numConvertido= int.Parse(numText);
+                Console.WriteLine("numero {0} convertido",numConvertido);
+
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Numero {0} Invalido",numText);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //convierte texto a numero y maneja la excepcion
+
+            Convertir convertir = new Convertir();
+            convertir.Calcular("2");
+            convertir.Calcular("z");
+        }
+
+    }
+}
