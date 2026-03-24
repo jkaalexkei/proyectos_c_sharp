61e615a77d9ea48ffb5a148f01a6f8b4110f7ee1
jkaalekei <jkaalekei@gmail.com>
2026-03-24 16:04:27 -0500

    ejercicio_77_de_100

---
+++ ejerciciosVarios/ejercicio_76_de_100/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_76_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe un programa que utilice throw para relanzar una excepción
+            try
+            {
+                LanzarError();//llamamos la funcion que relanza una excepción
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+                
+            }
+        }
+
+        static void LanzarError()
+        {
+            try
+            {
+                throw new Exception("Error detectado desde la funcion");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Enviamos el error al main");
+                throw;//relanzamos la excepcion a traves del llamado de nuevo mediante el uso de throw
+            }
+        }
+    }
+}
---
+++ ejerciciosVarios/ejercicio_77_de_100/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_77_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe un metodo que lance una excepcion y manejala en otro lugar
+            try
+            {
+                LanzarExcepcion();//aqui manejamos la excepción que viene del metodo
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void LanzarExcepcion()
+        {
+            int numero = 2;
+            if (numero > 5)
+            {
+                throw new Exception("Lanzando excepcion desde el metodo, numero mayor a 5");//creamos la excepcion si el numero es mayor a 5
+            }
+            else
+            {
+                Console.WriteLine("Numero menor a 5");
+            }
+            
+
+        }
+    }
+}
