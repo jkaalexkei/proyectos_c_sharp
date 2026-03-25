9d08cb5d1a7a4126f69059fe4d719a93dfc834f2
jkaalekei <jkaalekei@gmail.com>
2026-03-24 20:36:51 -0500

    ejercicio_81_de_100

---
+++ ejerciciosVarios/ejercicio_81_de_100/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ejercicio_81_de_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //crear y escribir en un archivo ruta: C:\Users\JKAALEXKEI\source\repos\ejerciciosVarios\ejercicio_81_de_100\archivos\
+            string ruta = @"C:\\Users\\JKAALEXKEI\\source\\repos\\ejerciciosVarios\\ejercicio_81_de_100\\archivo\\archivo.txt";
+        
+            Archivo archivo = new Archivo();
+            archivo.crear_escribir(ruta, "Hola Mundo como estas");
+
+
+        }
+    }
+
+    class Archivo
+    {
+        public void crear_escribir(string ruta, string texto)
+        {
+            
+            File.WriteAllText(ruta,texto);//objeto File y su metodo escribir para crear el archivo y texto dentro del archivo
+   
+            
+            Console.WriteLine("Archivo y texto creado");
+        }
+    }
+}
