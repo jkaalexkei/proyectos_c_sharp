1deab770cebd1796a777af7aba566910309c06ff
jkaalekei <jkaalekei@gmail.com>
2026-03-24 21:07:25 -0500

    ejercicio_82_de_100

---
+++ ejerciciosVarios/ejercicio_82_de_100/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ejercicio_82_de_100
+{
+    class Archivo
+    {
+        public void leer(string ruta)//metodo para procesar la lectura del contenido de un archivo
+        {
+            if (File.Exists(ruta))//aqui validamos si el archivo existe
+            {
+                string contenido = File.ReadAllText(ruta);//aqui leemos el contenido del archivo
+                Console.WriteLine(contenido);//imprimimos el contenido del archivo.txt
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //escribe un programa para leer un archivo
+            string ruta = @"C:\Users\JKAALEXKEI\source\repos\ejerciciosVarios\ejercicio_82_de_100\archivo\archivo.txt";
+            Archivo archivo = new Archivo();
+            archivo.leer(ruta);//llamamos al metodo leer de la clase archivo
+        }
+    }
+}
---
+++ ejerciciosVarios/ejercicio_82_de_100/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ejercicio_82_de_100")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ejercicio_82_de_100")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("2fa86ee1-08f4-4739-96ac-61cdc3d06a28")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
